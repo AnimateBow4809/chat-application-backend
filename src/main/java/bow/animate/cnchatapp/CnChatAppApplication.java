419c4358d6a87bfb6ca99a37b4e1d3efe9a39ac0
AnimateBow4809 <alidashtbozorg709@gmail.com>
2024-06-27 00:52:36 +0330

    init

---
+++ src/main/java/bow/animate/cnchatapp/CnChatAppApplication.java
@@ -0,0 +1,13 @@
+package bow.animate.cnchatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnChatAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CnChatAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bow/animate/cnchatapp/CnChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package bow.animate.cnchatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnChatAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
